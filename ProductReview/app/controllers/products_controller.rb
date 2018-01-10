7b1c6af6b07037bac0fbd16fb6b050c9abc96312
debutler266 <32465329+debutler266@users.noreply.github.com>
2018-01-09 22:36:14 -0500

    created a form for signed in users to add a new product

--- ProductReview/app/controllers/products_controller.rb
+++ ProductReview/app/controllers/products_controller.rb
@@ -1,4 +1,19 @@
 class ProductsController < ApplicationController
   def index
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+  end
+
+private
+
+  def product_params
+    parans.require(:product).permit(:title, :description, :brand)
+  end
+
 end
