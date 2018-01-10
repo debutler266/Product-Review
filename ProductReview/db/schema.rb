0b1a17e6027dd5f9674b52b8e9a260304b3c55a8
debutler266 <32465329+debutler266@users.noreply.github.com>
2018-01-10 10:19:05 -0500

    Added Add_products path when a user is currently signed in that has the root path to take users to the corresponding page

--- ProductReview/db/schema.rb
+++ ProductReview/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180110144205) do
+ActiveRecord::Schema.define(version: 20180110151605) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180110144205) do
     t.string "brand"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
