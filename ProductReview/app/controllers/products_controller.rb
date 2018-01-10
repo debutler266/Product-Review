e254aabbde536eb9212c282d05b7ebecae1c943b
debutler266 <32465329+debutler266@users.noreply.github.com>
2018-01-09 23:05:48 -0500

    when users adds a product, the corresponding index view will display each product in order

--- ProductReview/app/controllers/products_controller.rb
+++ ProductReview/app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all.order("created_at DESC")
   end
 
   def new
@@ -8,12 +9,18 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
 private
 
   def product_params
-    parans.require(:product).permit(:title, :description, :brand)
+    params.require(:product).permit(:title, :description, :brand)
   end
 
 end
